b7cbacedda8c6b3d0fac6632883d4346990dbf45
Gustavo Salmazo <gux.salmazo@gmail.com>
2022-01-19 14:46:07 -0300

    boardgame and chess classes created

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,13 @@
 package main;
 
+import boardgame.Board;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-
+		Board board = new Board(8,8);
+		System.out.println(board);
 	}
 
 }
